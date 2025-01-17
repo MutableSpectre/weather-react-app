8845827db1e983d6019d2749d80f5af9b6970164
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-16 22:55:31 -0500

    Fixed, Added, Removed
    Added the Weather css and js files. Fixed codes in the index and app files.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import "./App.css"
+import Weather from './Weather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <Weather />
+    </div>
   </React.StrictMode>
 );
 
