6be0d4acabc52a92558b2a79be9b60fd55eabd8e
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-19 10:55:46 -0500

    Condensed Code Down
    Weather.js and Weather.css were removed. The weather app code inside of them was moved to App.js and App.css.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-
-import "./App.css"
-import Weather from './Weather';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="App">
-      <Weather />
+      <App />
     </div>
   </React.StrictMode>
 );
