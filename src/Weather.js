6d0e4ac648f966f58830bfded7608bc108eebce8
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-01-19 07:25:40 -0500

    Removed and Added
    Removed unnecessary code and refined footer.

--- src/Weather.js
+++ src/Weather.js
@@ -46,8 +46,8 @@ export default function Weather() {
       </main>
       <footer className="contact-info">
         <p>
-          This project was coded by
-          <a href="https://github.com/MutableSpectre" target="_blank" rel="noreferrer"> Stephanie Ratliff-Putney</a> and is on
+          Open source code by
+          <a href="https://github.com/MutableSpectre" target="_blank" rel="noreferrer"> Stephanie Ratliff-Putney</a> that is on
           <a href="https://github.com/MutableSpectre/My-Weather-Application-SRP" target="_blank" rel="noreferrer"> GitHub</a> and
           hosted on <a href="https://my-weather-application-srp.netlify.app/" target="_blank" rel="noreferrer">  Netlify.</a>
         </p>
